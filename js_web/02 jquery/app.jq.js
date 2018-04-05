9ff004535a4fe7e6f3efc6532eba837e28b762d5
nuriagcmt <nuriagarmar@hotmail.com>
2018-04-05 21:28:35 +0200

    nuriagcmt

---
+++ js_web/02 jquery/app.jq.js
@@ -0,0 +1,17 @@
+/* $(document).ready(
+    function(){
+
+    }
+) */
+
+$(function() {
+    $('#btnSaludar').on('click', function() {
+        let user = $('#inpName').val()
+        console.log(user)
+        $('#outSaludo').html(`<p>Hola ${user}</p>`)
+    }) 
+    $('#btnBorrar').on('click', function() {
+        $('#inpName').val('')
+        $('#outSaludo').html('')
+    }) 
+})
